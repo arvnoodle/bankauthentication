444e4b2e3ad9db1155de88bc4316b404599dfdf8
arvnoodle <76831102+arvnoodle@users.noreply.github.com>
2022-03-21 16:16:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def predict_banknote(variance: int = Form(...), skewness: int = Form(...),
 
 # 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
-# if __name__ == '__main__':
-#     uvicorn.run(app, host='127.0.0.1', port=8000)
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
     
 #uvicorn app:app --reload
