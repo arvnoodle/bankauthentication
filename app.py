4278eaf79e3cb9ba6b508d43af0b41a32d810e0f
arvnoodle <76831102+arvnoodle@users.noreply.github.com>
2022-03-21 16:33:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,20 +10,7 @@ app = FastAPI()
 pickle_in = open("classifier.pkl","rb")
 classifier=pickle.load(pickle_in)
 
-# 3. Index route, opens automatically on http://127.0.0.1:8000
-@app.get('/')
-def index():
-    return {'message': 'Hello, World'}
-
-# 4. Route with a single parameter, returns the parameter within a message
-#    Located at: http://127.0.0.1:8000/AnyNameHere
-@app.get('/{name}')
-def get_name(name: str):
-    return {'Welcome': f'{name}'}
-
-# 3. Expose the prediction functionality, make a prediction from the passed
-#    JSON data and return the predicted Bank Note with the confidence
-@app.post('/predict')
+@app.post('/')
 def predict_banknote(variance: int = Form(...), skewness: int = Form(...),
                      curtosis: int = Form(...), entropy: int = Form(...)):
 
@@ -43,7 +30,7 @@ def predict_banknote(variance: int = Form(...), skewness: int = Form(...),
 
 # 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+# if __name__ == '__main__':
+#     uvicorn.run(app, host='127.0.0.1', port=8000)
     
 #uvicorn app:app --reload
